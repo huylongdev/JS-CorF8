ab1dbb6c779c00e6630da4b4291631835886ef82
huylongdev <haolong2004crtt@gmail.com>
2026-05-21 16:48:03 +0700

    additional

--- built-in/main.js
+++ built-in/main.js
@@ -4,6 +4,6 @@ setTimeout(function () {
   prompt("Please" + type);
 }, 2000);
 
-// setInterval(function () {
-//   console.log(type);
-// }, 2000);
+setInterval(function () {
+  console.log(type);
+}, 2000);
